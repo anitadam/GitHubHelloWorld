eeaa4f80aa2c37b4eca5340d6a77dce5f720ce84
Anita Dam <anitakdam@hotmail.com>
2025-04-14 14:13:54 +0200

    Hello World! How are you

--- src/common/HelloWorld.java
+++ src/common/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Dogs!");
         System.out.println("Hello Cats!");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!! How are you?");
 
     }
 }
