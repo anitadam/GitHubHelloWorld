2e8d3b63c52fcc01aa794eec8bdc190f28dbad59
anitadam <anitakdam@hotmail.com>
2025-04-14 14:09:56 +0200

    Update HelloWorld.java
    Dogs and cats

--- src/common/HelloWorld.java
+++ src/common/HelloWorld.java
@@ -4,8 +4,8 @@ import java.net.ServerSocket;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Dog!");
-        System.out.println("Hello Cat!");
+        System.out.println("Hello Dogs!");
+        System.out.println("Hello Cats!");
         System.out.println("Hello World!");
 
     }
